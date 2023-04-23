8ee0843db705e0deb6f09708347ea99f6830ae0c
Babatunde-Jesunsin <52846018+Babatunde-Jesunsin@users.noreply.github.com>
2023-04-23 20:43:33 +0100

    Removed Comment

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace PayAPI
 {
     public class Program
     {
-        //
+        
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
